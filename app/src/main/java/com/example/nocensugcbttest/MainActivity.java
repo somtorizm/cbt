9d6264b2d09be5db173db20c24677d78730729e9
somtorizm <ezinwavictor25@gmail.com>
2021-10-05 17:29:00 +0100

    Updated Ui

--- app/src/main/java/com/example/nocensugcbttest/MainActivity.java
+++ app/src/main/java/com/example/nocensugcbttest/MainActivity.java
@@ -1,28 +1,167 @@
 package com.example.nocensugcbttest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.DialogInterface;
+import android.database.DataSetObserver;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
-import android.widget.Adapter;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import link.fls.swipestack.SwipeStack;
 
 public class MainActivity extends AppCompatActivity {
         ViewPager viewPager;
+        PagerAdapter adapter;
+        ProgressBar progressBar;
+        Toolbar toolbar;
+        DialogInterface.OnClickListener clickListener;
+        Button next,previous;
+        TextView Question_no;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Make Device Full Screen
+        Window window = getWindow();
+
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        setLightStatusBar(window.getDecorView(), window);
+
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.app_bar);
+        toolbar.setTitle("");
+        toolbar.setSubtitle("Psychology (104)");
+        setSupportActionBar(toolbar);
+
+
         viewPager = findViewById(R.id.viewPager);
-        List<String> vic = new ArrayList<>();
-        vic.add("somto");
-        vic.add("junior");
-        viewPager.setAdapter(new SwipeStackAdapter(vic,this));
+        next = findViewById(R.id.next_button);
+        previous = findViewById(R.id.previous_btn);
+        progressBar = findViewById(R.id.progress_bar);
+        Question_no = findViewById(R.id.question_no);
+
+        List<Questions> vic = new ArrayList<>();
+        Questions ques1 = new Questions();
+        Questions questions2 = new Questions();
+        questions2.setQuestion("Francis (2006) categorized universal human rights into Francis (2006) categorized universal human rights into Francis (2006) categorized universal human rights into Francis (2006) categorized universal human rights into Francis (2006) categorized universal human rights into Francis (2006) categorized universal human rights into Francis (2006) categorized universal human rights into ?________");
+        questions2.setAns2("2");
+        questions2.setAns3("4");
+        questions2.setAns4("5");
+
+        ques1.setAns1("3");
+        ques1.setQuestion("Francis (2006) categorized universal human rights into ?________");
+        ques1.setAns2("2");
+        ques1.setAns3("4");
+        ques1.setAns4("5");
+        vic.add(ques1);
+        vic.add(questions2);
+        vic.add(ques1);
+        vic.add(questions2);
+        adapter = new SwipeStackAdapter(vic,this);
+
+
+
+
+
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                if(position == 0) previous.setVisibility(View.INVISIBLE);
+                else previous.setVisibility(View.VISIBLE);
+                if(position == vic.size()-1) next.setText("Finish");
+                else next.setText("Next");
+                if(position == 0 && position ==vic.size()-1){
+                    next.setVisibility(View.VISIBLE);
+                    next.setText("Finish");
+                }
+                progressBar.setProgress(position);
+                setQuestion_no(Question_no,position,vic.size());
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        int pos = viewPager.getCurrentItem();
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int current_position = viewPager.getCurrentItem();
+                if(current_position <= vic.size()){
+                    viewPager.setCurrentItem((current_position +1));
+                }
+            }
+        });
+        previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int current_position = viewPager.getCurrentItem();
+                if(current_position != 0 && current_position <= vic.size()){
+                    viewPager.setCurrentItem((current_position -1));
+                }
+            }
+        });
+
+        progressBar.setMax(vic.size()-1);
+
+
+
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+
+        return true;
+    }
+    public static void setLightStatusBar(View view, Window window) {
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            int flags = view.getSystemUiVisibility();
+            flags |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+            view.setSystemUiVisibility(flags);
+
+            window.setStatusBarColor(Color.WHITE);
+        }
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.setting:
+                Toast.makeText(this, "Clicked Settings", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return true;
+    }
+    private void setQuestion_no(TextView view, int no, int total){
+        view.setText(String.format("Question :%d/%d", (no+1), total));
+    }
 }
--- app/src/main/java/com/example/nocensugcbttest/SwipeStackAdapter.java
+++ app/src/main/java/com/example/nocensugcbttest/SwipeStackAdapter.java
@@ -1,25 +1,33 @@
 package com.example.nocensugcbttest;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.BaseAdapter;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
 
 import java.util.List;
 
-public class SwipeStackAdapter extends PagerAdapter  {
-    private List<String> mData;
+public class SwipeStackAdapter extends PagerAdapter{
+    private List<Questions> mData;
+    RadioButton btn1,btn2,btn3,btn4;
+    TextView question_txt;
     Context context;
+    boolean hasSelected = false;
 
 
-    public SwipeStackAdapter(List<String> mData, Context context) {
+    public SwipeStackAdapter(List<Questions> mData, Context context) {
         this.mData = mData;
         this.context = context;
     }
@@ -33,21 +41,47 @@ public class SwipeStackAdapter extends PagerAdapter  {
     @NonNull
     @Override
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
        View view = inflater.inflate(R.layout.slide,container,false);
-        container.addView(view);
+       question_txt = view.findViewById(R.id.question_txt_view);
+
+
+       btn1 = view.findViewById(R.id.radioButton2);
+       btn2 = view.findViewById(R.id.radioButton3);
+       btn3  = view.findViewById(R.id.radioButton4);
+       btn4 = view.findViewById(R.id.radioButton5);
+
+       Initialize(position);
+
+
+
+    container.addView(view);
         return  view;
     }
 
     @Override
     public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
-        container.removeView((FrameLayout) object);
+        container.removeView((LinearLayout) object);
     }
 
     @Override
     public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-        return (view == (FrameLayout) object);
+        return (view == (LinearLayout) object);
     }
+
+    private void Initialize(int pos){
+        question_txt.setText(mData.get(pos).getQuestion());
+        btn1.setText(mData.get(pos).getAns1());
+        btn2.setText(mData.get(pos).getAns2());
+        btn3.setText(mData.get(pos).getAns3());
+        btn4.setText(mData.get(pos).getAns4());
+
+
+    }
+
+
+
 
 
 
